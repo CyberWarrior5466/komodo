0d83e07f72b5e8ae380fb4b79fed81554ef6d7ca
Ali Saleem <86358454+CyberWarrior5466@users.noreply.github.com>
2026-02-05 11:01:58 +0000

    added button on top of source view

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,25 @@ use sourceview5::prelude::*;
 
 mod hover;
 
+fn main() {
+    let application = gtk::Application::new(
+        Some("com.github.bilelmoussaoui.sourceview5-example"),
+        Default::default(),
+    );
+    application.connect_activate(build_ui);
+
+    application.run();
+}
+
 fn build_ui(application: &gtk::Application) {
     let window = gtk::ApplicationWindow::new(application);
 
     window.set_title(Some("SourceView5 + Rust"));
     window.set_default_size(500, 500);
+
+    let container = gtk::Box::new(gtk::Orientation::Vertical, 0);
+    let button1 = gtk::Button::builder().label("click me").build();
+    container.append(&button1);
 
     let buffer = sourceview5::Buffer::new(None);
     buffer.set_highlight_syntax(true);
@@ -19,7 +33,7 @@ fn build_ui(application: &gtk::Application) {
         buffer.set_style_scheme(Some(scheme));
     }
 
-    let file = gio::File::for_path("../sourceview5/src/auto/buffer.rs");
+    let file = gio::File::for_path("buffer.rs");
     let file = sourceview5::File::builder().location(&file).build();
     let loader = sourceview5::FileLoader::new(&buffer, &file);
     loader.load_async_with_callback(
@@ -36,11 +50,6 @@ fn build_ui(application: &gtk::Application) {
         },
     );
 
-    let container = gtk::Box::new(gtk::Orientation::Horizontal, 0);
-    let scroll = gtk::ScrolledWindow::builder()
-        .vscrollbar_policy(gtk::PolicyType::External)
-        .build();
-
     let view = sourceview5::View::with_buffer(&buffer);
     view.set_monospace(true);
     view.set_background_pattern(sourceview5::BackgroundPatternType::Grid);
@@ -48,25 +57,15 @@ fn build_ui(application: &gtk::Application) {
     view.set_highlight_current_line(true);
     view.set_tab_width(4);
     view.set_hexpand(true);
+
+    let scroll = gtk::ScrolledWindow::builder()
+        .vexpand(true)
+        .vscrollbar_policy(gtk::PolicyType::External)
+        .build();
+
     scroll.set_child(Some(&view));
     container.append(&scroll);
-    let map = sourceview5::Map::new();
-    map.set_view(&view);
-    container.append(&map);
-
-    let provider = CustomHoverProvider::new();
-    view.hover().add_provider(&provider);
 
     window.set_child(Some(&container));
-    window.show();
-}
-
-fn main() {
-    let application = gtk::Application::new(
-        Some("com.github.bilelmoussaoui.sourceview5-example"),
-        Default::default(),
-    );
-    application.connect_activate(build_ui);
-
-    application.run();
+    window.present();
 }
